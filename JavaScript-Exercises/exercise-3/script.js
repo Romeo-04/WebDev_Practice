62e46ffae6c17601018bf7d9a9caf1e5874e3246
Romeo-04 <jatolentino@fit.edu.ph>
2025-09-26 20:31:29 +0800

    Added a feature that stops the game if either computer or player reached 10 points, declaring the winner and ending the game

--- JavaScript-Exercises/exercise-3/script.js
+++ JavaScript-Exercises/exercise-3/script.js
@@ -3,20 +3,31 @@ let playerScore = 0;
 let wins = 0;
 let losses = 0;
 let resultText = "";
+let gameStatus = "";
 
 function flipCoin(userGuess){
     const randomNum = Math.random();
     randomNum < 0.5? result = "heads" : result = "tails";
 
-    if (result === userGuess) {
+    if (result === userGuess && (wins < 10 && losses < 10)) {
         wins++;
         resultText = "You win! The coin landed on " + result;
-    } else {
+    } else if (result !== userGuess && (wins < 10 && losses < 10)) {
         losses++;
         resultText = "You lose! The coin landed on " + result;
+    } else if (wins === 10 || losses === 10) {
+        gameStatus = "Game over! Please reset the game to play again.";
     }
+
+    if (wins === 10 && losses !== 10) {
+        resultText = "Congratulations! You reached 10 wins first, you won!";
+    } else if (wins !== 10 && losses === 10) {
+        resultText = "Sorry! The computer reached 10 wins first, you lost!";
+    }
+    
 
     document.getElementById("result").innerText = resultText;
     document.getElementById("wins").innerText = wins;
     document.getElementById("losses").innerText = losses;
+    document.getElementById("game-status").innerText = gameStatus;
 }
