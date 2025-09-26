a81217f54cd0bde318a7906c6e5e7dfb35f76985
Romeo-04 <jatolentino@fit.edu.ph>
2025-09-26 19:12:19 +0800

    coin flip game is done

--- JavaScript-Exercises/exercise-3/script.js
+++ JavaScript-Exercises/exercise-3/script.js
@@ -1,7 +1,22 @@
 let result = "";
 let playerScore = 0;
+let wins = 0;
+let losses = 0;
+let resultText = "";
 
 function flipCoin(userGuess){
     const randomNum = Math.random();
     randomNum < 0.5? result = "heads" : result = "tails";
+
+    if (result === userGuess) {
+        wins++;
+        resultText = "You win! The coin landed on " + result;
+    } else {
+        losses++;
+        resultText = "You lose! The coin landed on " + result;
+    }
+
+    document.getElementById("result").innerText = resultText;
+    document.getElementById("wins").innerText = wins;
+    document.getElementById("losses").innerText = losses;
 }
