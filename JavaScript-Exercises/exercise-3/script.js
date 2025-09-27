8c0aac332d4e98c9ec84c4b9616b169de443305d
Romeo-04 <jatolentino@fit.edu.ph>
2025-09-27 20:04:06 +0800

    Added reset button for coin flip

--- JavaScript-Exercises/exercise-3/script.js
+++ JavaScript-Exercises/exercise-3/script.js
@@ -1,7 +1,6 @@
 let result = "";
 let playerScore = 0;
-let wins = 0;
-let losses = 0;
+const scores = {'wins':0,'losses':0}
 let resultText = "";
 let gameStatus = "";
 
@@ -9,25 +8,31 @@ function flipCoin(userGuess){
     const randomNum = Math.random();
     randomNum < 0.5? result = "heads" : result = "tails";
 
-    if (result === userGuess && (wins < 10 && losses < 10)) {
-        wins++;
+    if (result === userGuess && (scores.wins < 10 && scores.losses < 10)) {
+        scores.wins++;
         resultText = "You win! The coin landed on " + result;
-    } else if (result !== userGuess && (wins < 10 && losses < 10)) {
-        losses++;
+    } else if (result !== userGuess && (scores.wins < 10 && scores.losses < 10)) {
+        scores.losses++;
         resultText = "You lose! The coin landed on " + result;
-    } else if (wins === 10 || losses === 10) {
+    } else if (scores.wins === 10 || scores.losses === 10) {
         gameStatus = "Game over! Please reset the game to play again.";
     }
 
-    if (wins === 10 && losses !== 10) {
+    if (scores.wins === 10 && scores.losses !== 10) {
         resultText = "Congratulations! You reached 10 wins first, you won!";
-    } else if (wins !== 10 && losses === 10) {
+    } else if (scores.wins !== 10 && scores.losses === 10) {
         resultText = "Sorry! The computer reached 10 wins first, you lost!";
     }
     
 
     document.getElementById("result").innerText = resultText;
-    document.getElementById("wins").innerText = wins;
-    document.getElementById("losses").innerText = losses;
+    document.getElementById("wins").innerText = scores.wins;
+    document.getElementById("losses").innerText = scores.losses;
     document.getElementById("game-status").innerText = gameStatus;
+}
+
+function resetGame(){
+    document.getElementById('reset-button').addEventListener('click', () => {
+    window.location.reload(); // reloads the current URL)
+});
 }
