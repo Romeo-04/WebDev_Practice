0ced7541f78cfef2069938a2c5638567623583ea
Romeo-04 <jatolentino@fit.edu.ph>
2025-09-28 17:01:18 +0800

    done the exercise

--- JavaScript-Exercises/exercise-4/script.js
+++ JavaScript-Exercises/exercise-4/script.js
@@ -1,2 +1,16 @@
-document.body.innerHTML = '<h1>Hello, JavaScript!</h1>';
-console.log('Hello, JavaScript!');
+function subscribed(){
+    document.querySelector(".js-button").innerHTML = "Subscribed";
+}
+
+function calculateShipping(inp = 0){
+   const orderAmount = parseFloat(document.querySelector(inp).value) || 0;
+   let shippingCost = 0;
+
+   if (orderAmount < 40) {
+       shippingCost = 10;
+   }
+
+   result = orderAmount + shippingCost;
+   
+   document.getElementById("result").innerHTML = "Total Amount: $" + result;
+}
