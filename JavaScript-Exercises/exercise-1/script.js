cad36700f53153f84947799609c48b8f7e42dd38
Romeo-04 <jatolentino@fit.edu.ph>
2025-09-29 23:11:43 +0800

    almost finished

--- JavaScript-Exercises/exercise-1/script.js
+++ JavaScript-Exercises/exercise-1/script.js
@@ -1,1 +1,17 @@
 let calculation = "";
+
+function calculate(n){
+    calculation += n;
+    document.getElementById("display-input").innerText = calculation;
+}
+
+function clearCalculation(){
+    calculation = "";
+    document.getElementById("display-input").innerText = calculation;
+}
+
+function finalCalculate(){
+    calculation = eval(calculation);
+    document.getElementById("display-input").innerText = calculation;
+    calculation = "";
+}
