8b10c63b9db09177b558c9d0252c8a0093ae74fa
Romeo-04 <jatolentino@fit.edu.ph>
2025-10-08 00:52:04 +0800

    jgb

--- JavaScript-Exercises/exercise-5/script.js
+++ JavaScript-Exercises/exercise-5/script.js
@@ -1,7 +1,10 @@
 function subscribe(){
-    if (document.querySelector('.subscribe-button').innerText === "Subscribe") {
-        button.classlist.add('.subscribed');
-    
+    if (document.querySelector('.js-subscribe-button').innerText === "Subscribe") {
+        document.querySelector('.js-subscribe-button').classList.add('is-subscribed');
+        document.querySelector('.js-subscribe-button').innerHTML = "Subscribed";
     }
-
+    else if (document.querySelector('.js-subscribe-button').innerText === "Subscribed") {
+        document.querySelector('.js-subscribe-button').classList.remove('is-subscribed');
+        document.querySelector('.js-subscribe-button').innerHTML = "Subscribe";
+    }
 }
