26a5bb6dcd16c7a92ddbfe59327d28430bbd9a4c
Romeo-04 <jatolentino@fit.edu.ph>
2025-10-08 16:12:34 +0800

    added shipping calculator

--- JavaScript-Exercises/exercise-5/script.js
+++ JavaScript-Exercises/exercise-5/script.js
@@ -8,3 +8,20 @@ function subscribe(){
         document.querySelector('.js-subscribe-button').innerHTML = "Subscribe";
     }
 }
+function calculateShipping(){
+    let price = document.getElementById("price").value;
+    let shipping = 0;
+
+    if (price < 50){
+        shipping = 10;
+        document.getElementById("shipping-cost").innerHTML = "Total Cost: " + eval(Number(price) + 10);
+    }
+
+    else if (price >= 50){
+        document.getElementById("shipping-cost").innerHTML = "Total Cost: " + price;
+    }
+
+    else if (price <= 0){
+        document.getElementById("shipping-cost").innerHTML = "Please enter a valid price";
+    }
+}
