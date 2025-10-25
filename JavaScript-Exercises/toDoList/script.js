b298fe759a40219e1a3cc0e9834cd7340d1902d2
Romeo-04 <jatolentino@fit.edu.ph>
2025-10-25 20:56:25 +0800

    learning to troubleshoot my own implementation

--- JavaScript-Exercises/toDoList/script.js
+++ JavaScript-Exercises/toDoList/script.js
@@ -9,10 +9,37 @@ function addTask(){
 
     if(task){
         todoList.push(task);
-        todoListHTML += `<li>${task}</li>`;
+        let i = todoList.length - 1;
+
+        todoListHTML += `<li>${task}</li> <button onclick="deleteTask(${i});
+        "> Delete </button>`;
+
+        console.log(todoListHTML);
+        console.log(todoList);
         document.getElementById("todoListHTML").innerHTML = todoListHTML;
     }
+
     else {
         alert('Enter task monkey');
     }
 } 
+
+function deleteTask(e){
+    for(let i = 0; i < todoList.length; i++){
+        if(i === e){
+            todoList.splice(i,1);
+            console.log(todoList);
+        }
+    }
+    displayTask();
+
+}
+
+function displayTask(){
+    for(let i = 0; i < todoList.length; i++){
+        let task = todoList[i];
+        let i = todoList.length - 1;
+
+        todoListHTML += `<li>${task}</li> <button onclick="deleteTask(${i})`;
+    }
+}
